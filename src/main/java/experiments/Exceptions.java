a7bd03fafa84f0cb825e4de14b60973e97cb693f
nikmac <nikita.vasyuhno1337@gmail.com>
2025-05-15 12:50:47 +0300

    Create Exceptions

--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -1,2 +1,24 @@
-package experiments;public class Exceptions {
+package experiments;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray = {"str1","str2","str3"};
+        uncheckedException(strArray);
+    }
+    private static void uncheckedException(String[] strArray)
+    {
+        try{
+            for (int i = 0; i < 5; i++) {
+            System.out.println(strArray[i]);
+        }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("create exception");
+        }
+        System.out.println("program is working");
+    }
+
+    private static void pause() throws InterruptedException {
+        Thread.sleep(3000);
+    }
 }
